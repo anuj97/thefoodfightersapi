2bd4a86887192edecd4b171f00066c3a366b393c
anuj97 <anujupadhyayjnp@gmail.com>
2020-06-23 16:56:18 +0530

    cors

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 4000
 
-express.static('/')
+// app.use(cors())
 
+
+
+// var corsOptions = {
+//     origin: '*',
+//     optionsSuccessStatus: 200
+// }
+
+app.options('/images/', function (req, res, next) {
+    console.log('Pre-Flight Test')
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept, X-Requested-With, Access-Control-Allow-Origin");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.status(200);
+    res.end()
+})
+
+
+express.static('/')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => res.send('Hello World! Get.'))
+
+// app.options('/images/', cors())
+
+app.get('/images', function (req, res, next) {
+    res.set('Access-Control-Allow-Origin', '*')
+    res.set('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept, X-Requested-With, Access-Control-Allow-Origin')
+    res.send([
+        '/images/elaicheesecake.png',
+        '/images/uthappizza.png',
+        '/images/vadonut.png'
+    ]
+)})
 
 app.post('/', (req, res) => res.send('Hello World! Post.'))
 
 app.put('/', (req, res) => res.send('Hello World! Put.'))
 
 app.delete('/', (req, res) => res.send('Hello World! Delete.'))
+
+app.use((req, res, next) => res.status(404).send("Sorry can't find that!"))
+
+app.use( (err, req, res, next) => {
+    // console.log(err)
+
+    res.status(500).send('Something broke!')
+  })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
